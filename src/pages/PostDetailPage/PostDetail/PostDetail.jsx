5feaabe9f20274f2cdfca36fdb455b9e36d00907
hong jun ki <ghdwnsrl1991@gmail.com>
2025-01-27 01:55:09 +0900

    feat. post title 을 브라우저 창 제목으로 설정

--- src/pages/PostDetailPage/PostDetail/PostDetail.jsx
+++ src/pages/PostDetailPage/PostDetail/PostDetail.jsx
@@ -3,12 +3,20 @@ import {getByPostId} from "../../../api/posts.js";
 import {useSuspenseQuery} from "@tanstack/react-query";
 import LikeButton from "./LikeButton.jsx";
 import PostDetailHeader from "./PostDetailHeader.jsx";
+import {useEffect} from "react";
 
 const PostDetail = ({id}) => {
     const { data : post } = useSuspenseQuery({
         queryKey: ["Post", id],
         queryFn: getByPostId
     })
+
+    useEffect(() => {
+        document.title = post.title;
+        return () => {
+            document.title = "Mulkkogi";
+        };
+    }, [post.title]);
 
     return <>
         <p className='text-2xl font-bold'>{post.title}</p>
