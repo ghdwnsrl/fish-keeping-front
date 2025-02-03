14558ec43626a00f8ef8bfe62e694b3b49c027a6
hong jun ki <ghdwnsrl1991@gmail.com>
2025-02-03 15:19:58 +0900

    fix. 서버로 받은 최신 글의 thumbnailUrl 렌더링

--- src/pages/UserPage/TankCard.jsx
+++ src/pages/UserPage/TankCard.jsx
@@ -1,20 +1,20 @@
 
 import {Link} from "react-router-dom";
 
-function TankCard({ username, imgURL='https://placehold.co/200x240?text=NO%20IMAGE', props}) {
-    const {name, id, description, lastModified = '-', totalPosts} = props
-    console.log('tankCard ', username, name)
+function TankCard({ username, props}) {
+    console.log(props)
+    const {name, id, thumbnailUrl, description, lastModified, totalPosts} = props
     return (
         <Link to={`/users/${username}/archives/${name}`}>
             <div key={id}>
-                <img className='object-cover w-48 h-48 sm:h-64 sm:w-64 rounded-lg' src={imgURL}
+                <img className='object-cover w-48 h-48 sm:h-64 sm:w-64 rounded-lg' src={thumbnailUrl ?? 'https://placehold.co/200x240?text=NO%20IMAGE'}
                      alt='thumbnail'/>
                 <dl key={name} className='m-1'>
                     <dt className='font-bold'>{name}</dt>
                     <dd>{description}</dd>
                     <div className='flex-row flex-grow gap-1 sm:flex sm:items-end'>
                         <p className='text-sm'>{totalPosts} 개의 업로드</p>
-                        <p className='text-xs text-gray-500'>마지막 업로드 {lastModified}</p>
+                        <p className='text-xs text-gray-500'>마지막 업로드 {lastModified?? '-'}</p>
                     </div>
                 </dl>
             </div>
