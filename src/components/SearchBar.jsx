968b401f3efaee69fb0a52fd005dbe2eabb75b26
hong jun ki <ghdwnsrl1991@gmail.com>
2025-02-10 00:22:06 +0900

    refactor. 검색 컴포넌트 useState -> useForm 사용

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,54 +1,44 @@
 import {FaMagnifyingGlass} from "react-icons/fa6";
 import {Select} from "@headlessui/react";
-import {useState} from "react";
 import Button from "./Button.jsx";
 import {useNavigate} from "react-router-dom";
+import {Controller, useForm} from "react-hook-form";
+import Form from "./Form.jsx";
 
 const SearchBar = ({condition, children, onSearchBarClickHandler}) => {
 
+    const { handleSubmit, control, register} = useForm({
+        defaultValues: {
+            type: condition ? condition.type : "title",
+            keyword: condition ? condition.keyword : ""
+        }});
     const navigate = useNavigate()
 
-    const [value, setValue] = useState({
-        type: condition ? condition.type : "title",
-        keyword: condition ? condition.keyword : ""
-    })
-
-    const handleOnChangeCondition = (e) => {
-        const {name, value} = e.target;
-        setValue((prevState) => ({
-            ...prevState,
-            [name]: value
-        }))
-    }
-
-    const updateStateAndNavigate = (value) => {
+    const handleClick = (value) => {
         onSearchBarClickHandler(value);
         navigate(`/search?type=${value.type}&keyword=${value.keyword}`)
     }
 
-    const handleClick = () => {
-        updateStateAndNavigate(value)
-    }
-
-    const handleKeyDown = (e) => {
-        if (e.key === "Enter" && !e.nativeEvent.isComposing) {
-            updateStateAndNavigate(value)
-        }
-    }
-
-    return <div className='flex gap-5 justify-center pt-5 flex-wrap'>
-        <Select name="type"
-                value={value.type}
-                onChange={handleOnChangeCondition}
-                className='border rounded-xl px-1 w-55 h-10'
-        >
-            {children}
-        </Select>
+    return <Form styleType='flex gap-5 justify-center pt-5 flex-wrap' handleSubmit={handleSubmit(handleClick)}>
+        <Controller control={control} name="type"
+            render={({field: {value, onChange}}) => {
+                return (
+                    <Select name="type"
+                            className='border rounded-xl px-1 w-55 h-10'
+                            onChange={onChange}
+                            value={value}
+                            aria-label="검색 타입 선택"
+                    >
+                        {children}
+                    </Select>
+                )
+        }}
+        />
         <div className='flex border p-2 rounded-xl w-55 h-10'>
-            <input className='outline-none' name='keyword' onKeyDown={handleKeyDown} value={value.keyword} onChange={handleOnChangeCondition}/>
-            <Button styleType='px-1 border-none' onClick={handleClick}>{<FaMagnifyingGlass/>}</Button>
+            <input className='outline-none' aria-label="검색어" name='keyword' {...register("keyword", {required: true})}/>
+            <Button aria-label='검색' styleType='px-1 border-none'>{<FaMagnifyingGlass/>}</Button>
         </div>
-    </div>
+    </Form>
 
 }
 
