8875b86b1aa0ea532dd256e5db5e3d7c0081a255
hong jun ki <ghdwnsrl1991@gmail.com>
2025-03-24 19:12:21 +0900

    feat. redux-persist 데이터 암호화

--- src/feature/store.js
+++ src/feature/store.js
@@ -3,11 +3,20 @@ import authReducer from './authSlice';
 import storage from 'redux-persist/lib/storage';
 import {persistReducer, persistStore} from "redux-persist";
 import dialogReducer from "./dialogSlice"
+import {encryptTransform} from "redux-persist-transform-encrypt";
 
 const persistConfig = {
     key: 'root',
     storage,
-    whitelist: ['auth']
+    whitelist: ['auth'],
+    transforms: [
+        encryptTransform({
+            secretKey: import.meta.env.VITE_SECURE_LOCAL_STORAGE_HASH_KEY,
+            onError: function (error) {
+                console.error(error)
+            },
+        }),
+    ],
 }
 
 const rootReducer = combineReducers({
--- src/pages/admin/AdminLoginPage/index.jsx
+++ src/pages/admin/AdminLoginPage/index.jsx
@@ -1,17 +1,14 @@
 import LoginForm from "../../../components/LoginForm.jsx";
-import {loginSuccess} from "../../../feature/authSlice.js";
-import {useNavigate} from "react-router-dom";
 import {useDispatch} from "react-redux";
+import {useNavigate} from "react-router-dom";
 import {loginAdmin} from "../../../api/user.js";
 
 const AdminPage = () => {
-
-    const navigate = useNavigate()
     const dispatch = useDispatch();
-
-    const onSuccess = (data, variables) => {
-        dispatch(loginSuccess(variables.username));
-        navigate('/admin/dashboard');
+    const navigate = useNavigate();
+    const onSuccess = () => {
+        // dispatch(loginAdminSuccess());
+        navigate('/');
     }
     return (
         <div>
